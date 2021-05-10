04723bbb0dd5dd2272660f1f7c639a3d7c8485a8
Whitney Wong <whitneywong@newemplyee22MBP.grandenetworks.net>
2021-05-10 18:57:24 -0500

    Tutorial lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,33 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = 'yoshi'
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+/*****  Arrays *****/
+// Note: Implicit type set if an array is initialized with elements of one type (e.g. all strings or all numbers). When trying to push or modify an array with implicit types with values not of that type, ts won't allow you to compile the script. If array is initialized with mixed types, then you can modify the array with those types that are specified.
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(74);
+// names[0] = 3;
+// names = 'hello';
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shawn');
+// numbers[1] = 'sean';
+var mixed = ['key', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(31);
+mixed[0] = 3;
+// mixed.push(true)
+/*****  Objects *****/
+// Note: When initializing an object, type properties are implicitly set for each key in the object. Cannot modify the object property with different type or add a new property.
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+// ninja = 'hi';
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 10
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,40 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+/*****  Arrays *****/
+// Note: Implicit type set if an array is initialized with elements of one type (e.g. all strings or all numbers). When trying to push or modify an array with implicit types with values not of that type, ts won't allow you to compile the script. If array is initialized with mixed types, then you can modify the array with those types that are specified.
+let names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(74);
+// names[0] = 3;
+// names = 'hello';
 
-// character = 20;
-character = 'luigi';
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shawn');
+// numbers[1] = 'sean';
 
-// age = 'yoshi'
-age = 40;
+let mixed = ['key', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(31);
+mixed[0] = 3;
+// mixed.push(true)
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI
-}
+/*****  Objects *****/
+// Note: When initializing an object, type properties are implicitly set for each key in the object. Cannot modify the object property with different type or add a new property.
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
 
-console.log(circ(7.5))
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+// ninja = 'hi';
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 10, 
+  // skills: ['hyah', 'grrr']
+};
