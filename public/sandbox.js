c11cc245bbae602f8fb576ef2364b27762fb9844
Whitney Wong <whitneywong@newemplyee22MBP.grandenetworks.net>
2021-05-12 08:47:47 -0500

    Tutorial lesson 9. Aliases.

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,26 +1,22 @@
 "use strict";
-// Can explicity state 'Function' type
-var greet;
-// greet = 'hello';  --> Cannot re-assign greet function to a string value.
-greet = function () {
-    console.log("hello, world");
+/* Original */
+/*
+  const logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+  }
+*/
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-// Can create optional parameters by adding '?' next to the parameter
-var multiply = function (a, b, c) {
-    console.log(a + b);
-    console.log(c);
+/* Original */
+/*
+  const greet = (user: {name: string, uid: string | number}) => {
+    console.log(`${user.name} says hello`);
+  }
+*/
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-// Can give a parameter a default value --> similar to declaring a variable with a value. When there is no return value, typescript interprets the function to return void (or nothing).
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
+var greetAgain = function (user) {
+    console.log(user.name + " says hello");
 };
-add(30, 1);
-add(5, 10, '20');
-// Can define what the expected return value is by adding a data type after function parentheses. Depending on function setup, typescript can infer the data type and there's no reason to explicitly set data type for the function.
-var minus = function (a, b) {
-    return a - b;
-};
-var result = minus(10, 7);
-// result = 'something else';
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,28 +1,27 @@
-// Can explicity state 'Function' type
-let greet: Function;
-// greet = 'hello';  --> Cannot re-assign greet function to a string value.
-greet = () => {
-  console.log("hello, world");
+// Type Aliases
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum };
+
+/* Original */
+/* 
+  const logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+  }
+*/
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
 }
 
-// Can create optional parameters by adding '?' next to the parameter
-const multiply = (a: number, b: number, c?: number|string) => {
-  console.log(a + b);
-  console.log(c);
+/* Original */
+/* 
+  const greet = (user: {name: string, uid: string | number}) => {
+    console.log(`${user.name} says hello`);
+  }
+*/
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
 }
 
-// Can give a parameter a default value --> similar to declaring a variable with a value. When there is no return value, typescript interprets the function to return void (or nothing).
-const add = (a: number, b: number, c: number|string = 10) => {
-  console.log(a + b);
-  console.log(c);
+const greetAgain = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
 }
-add(30, 1);
-add(5, 10, '20');
-
-// Can define what the expected return value is by adding a data type after function parentheses. Depending on function setup, typescript can infer the data type and there's no reason to explicitly set data type for the function.
-const minus = (a: number, b: number): number => {
-  return a - b;
-}
-
-let result = minus(10, 7);
-// result = 'something else';
