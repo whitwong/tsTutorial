10dca5f8cd15005a8d09a07237a250c1b6a3071b
Whitney Wong <whitneywong@newemplyee22MBP.grandenetworks.net>
2021-05-13 17:20:25 -0500

    Tutorial lesson 13 - Access Modifiers.

--- public/app.js
+++ public/app.js
@@ -1,26 +1,37 @@
 "use strict";
-// classes
+// classes and Access Modifiers
+// Note: When you make a property private, that property is only accessible within the class (e.g. cannot console.log outside the class, but this.details in the class is acceptable). Read-only property makes it accessible outside of the class but cannot modify that property.
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    // constructor(c: string, d: string, a: number) {
+    //   this.client = c;
+    //   this.details = d;
+    //   this.amount = a;
+    // }
+    // Note: This way of defining a constructor only works if you add the access modifiers. Will cause errors if you don't have access modifiers in front of properties.
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
+        // this.client = 'something else';
         return this.client + " owes $" + this.amount + " for " + this.details;
     };
     return Invoice;
 }());
 // instantiate the class and create an object based on it
-var invOne = new Invoice('Yoshi', 'work on the Mario website', 250);
+var invOne = new Invoice('Mario', 'work on the Mario website', 250);
 var invTwo = new Invoice('Luigi', 'work on the Luigi website', 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = 'Mario';
-invTwo.amount = 400;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(function (inv) {
+    // invOne.client = 'something else';
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector('.new-item-form');
 // inputs
 var type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,41 @@
-// classes
+// classes and Access Modifiers
+// Note: When you make a property private, that property is only accessible within the class (e.g. cannot console.log outside the class, but this.details in the class is acceptable). Read-only property makes it accessible outside of the class but cannot modify that property.
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  // constructor(c: string, d: string, a: number) {
+  //   this.client = c;
+  //   this.details = d;
+  //   this.amount = a;
+  // }
+
+  // Note: This way of defining a constructor only works if you add the access modifiers. Will cause errors if you don't have access modifiers in front of properties.
+  constructor( 
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
+    // this.client = 'something else';
     return `${this.client} owes $${this.amount} for ${this.details}`;
   }
 }
 
 // instantiate the class and create an object based on it
-const invOne = new Invoice('Yoshi', 'work on the Mario website', 250);
+const invOne = new Invoice('Mario', 'work on the Mario website', 250);
 const invTwo = new Invoice('Luigi', 'work on the Luigi website', 300);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'Mario';
-invTwo.amount = 400;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(inv => {
+  // invOne.client = 'something else';
+  console.log(inv.client, inv.amount, inv.format());
+})
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
